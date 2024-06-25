418bca07a3cde0aa52631e0720ba224edb204660
mateocampo1004 <146900595+mateocampo1004@users.noreply.github.com>
2024-06-25 17:05:32 -0500

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
-import { AppComponent } from './app.component'; 
+import { AppComponent } from './app.component';
 import { CarFormComponent } from './components/car-form/car-form.component';
 import { CarListComponent } from './components/car-list/car-list.component';
 
@@ -20,6 +20,6 @@ import { CarListComponent } from './components/car-list/car-list.component';
     ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [] // Remove AppComponent from bootstrap array
+  bootstrap: [AppComponent] // Coloca AppComponent en el array bootstrap
 })
 export class AppModule { }
